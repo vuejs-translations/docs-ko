26e7f6c7c978ebf1a2cbc6deefafec54bbd23750
Niceplugin <niceplugin@gmail.com>
2024-03-31 18:48:59 +0900

    Add translation link to fa.vuejs.org (#2758)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -664,6 +664,11 @@ export default defineConfigWithTheme<ThemeConfig>({
         repo: 'https://github.com/vuejs-translations/docs-it'
       },
       {
+        link: 'https://fa.vuejs.org',
+        text: 'فارسی',
+        repo: 'https://github.com/vuejs-translations/docs-fa'
+      },
+      {
         link: '/translations/',
         text: '번역에 참가하세요!',
         isTranslationsDesc: true
